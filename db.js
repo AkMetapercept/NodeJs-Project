e11b43f534ab0391cbfa51cc7e330d3eccef3e6e
AkMetapercept <159915532+AkMetapercept@users.noreply.github.com>
2024-02-23 11:32:11 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-
-const connectDb=async ()=>{
-    mongoose.connect("mongodb+srv://akshayrp:root@cluster.ar9cvdc.mongodb.net/").then(()=>{
-        console.log("Database connectd...");
-    })
-}
-module.exports=connectDb;
